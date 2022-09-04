0d1c6ef24b1ea3b1646b8b56a984e43d09cc968a
rampadmin <112690756+rampadmin@users.noreply.github.com>
2022-09-04 19:03:34 +0530

    Render landing page on default route

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+project_root = os.path.dirname(__file__)
+static_folder = os.path.join(project_root, 'assets')
+template_path = os.path.join(project_root, 'templates')
+app = Flask(__name__, template_folder=template_path, static_folder=static_folder)
 
 @app.route('/')
 def landing_page():
-  return 'Landing page example'
+  return render_template('index.html')
 
 @app.route('/app')
 def main_app():
